5eaed0760813af2ca228c35ee182acc8282944ae
Rakshith <60108744+Rak28@users.noreply.github.com>
2020-06-08 12:02:38 +0530

    Data structures assignment
    Prints list, tuple, and dictionary

---
+++ data_structures.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  8 11:48:20 2020
+
+@author: Rakshith
+"""
+list = [1,2,3,4,5,6,7]
+print("Element 3 from the list : ", list[2])
+
+tuple = (1,2,3,4,5)
+print("Element 3 from the tuple: ", tuple[2])
+
+dic = {1:"One",2:"Two",3:"Three"}
+print("Element with key 3 from the dictionary: ", dic[3])
+
+
